29f36521b2b05891bd064eb966705a6ac2bf4b26
jmininel <ju.mininel@hotmail.com>
2023-09-19 15:11:36 -0300

    add: import postAvatr

--- src/app/(home)/post/post.tsx
+++ src/app/(home)/post/post.tsx
@@ -1,14 +1,12 @@
-import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+
 import { Button } from "@/components/ui/button";
 import { NewPost } from './newPost'
+import { PostAvatar } from "./post-avatar";
 
 export default function HomePost () {
   return(
     <div className="flex justify-between items-center gap-3">
-    <Avatar>
-      <AvatarImage src="https://github.com/shadcn.png" alt="@shadcn" />
-      <AvatarFallback>CN</AvatarFallback>
-    </Avatar>
+   <PostAvatar src="https://github.com/shadcn.png" fallbackInitials="Shadcn"/>
 
     <NewPost />
     <Button disabled variant="outline" className="rounded-xl text-neutral-500 ">
