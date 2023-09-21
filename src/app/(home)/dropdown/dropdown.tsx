83f2035f5bc4b85ff9aef59a181e1d28ba6887cc
jmininel <ju.mininel@hotmail.com>
2023-09-21 13:36:38 -0300

    feat: update page

--- src/app/(home)/dropdown/dropdown.tsx
+++ src/app/(home)/dropdown/dropdown.tsx
@@ -4,8 +4,6 @@ import * as React from "react"
 import { DropdownMenuCheckboxItemProps } from "@radix-ui/react-dropdown-menu"
 import { Settings2 } from "lucide-react"
 
-
-
 import {
   DropdownMenu,
   DropdownMenuCheckboxItem,
